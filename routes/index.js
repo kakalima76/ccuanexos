1e5a5ca6d2e6e15fc7550ca10c75487c73c44a66
Nieraldo da Silva Lima <nieraldo@gmail.com>
2016-01-27 12:26:37 -0200

    correção na man page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'F/CCU/GOP' });
 });
 
 module.exports = router;
