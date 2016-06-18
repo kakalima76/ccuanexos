1efba5d3e24cc6f18e350e83d885a77e04ee14bb
kakalima76 <nieraldo@gmail.com>
2016-06-18 16:05:05 -0300

    alteração na viewRouter index

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,6 @@ router.use('/autorizado', require('./autorizadoRouter'));
 router.use('/imagem', require('./fotoRouter'));
 router.use('/vistoria', require('./vistoriaRouter'));
 router.use('/ordem', require('./ordemRouter'));
-router.use('/cadastro', require('./inscritosRouter'));
-router.use('/numero', require('./numeroRouter'));
-router.use('/foto', require('./fotoInscritoRouter'));
 router.use('/cep', require('./cepRouter'));
 router.use('/lacre', require('./lacreRouter'));
 
